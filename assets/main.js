1fc6a4bf46850488ff6b0c1f2ee7f7d72703e1e5
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2023-06-19 23:42:11 -0500

    home page

--- assets/main.js
+++ assets/main.js
@@ -6,7 +6,7 @@ var closeMenu = document.querySelector(".close-drawer");
 responsiveMenu.style.height = screen.height;
 
 hamburguerMenu.addEventListener("click", function () {
-  responsiveMenu.style.display = "block";
+  responsiveMenu.style.display = "flex";
   responsiveMenu.style.opacity = 1;
   responsiveMenu.style.width = screen.width * 0.8;
 });
