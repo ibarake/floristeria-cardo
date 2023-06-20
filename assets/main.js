a58e298f2889470043a5b29559b8ace7badc3155
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2023-06-19 21:06:12 -0500

    home page

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   var hamburgerMenu = document.querySelector(".hamburger-menu");
   var responsiveMenu = document.querySelector(".responsive-menu");
+  var closeMenu = document.querySelector(".close-drawer");
 
   hamburgerMenu.addEventListener("click", function () {
     responsiveMenu.style.display = "block";
+  });
+  closeMenu.addEventListener("click", function () {
+    responsiveMenu.style.display = "none";
   });
 });
