182d3b4382d902b16a3dd529d64076dbc361df94
idelahoz14 <idelahoz14@cuc.edu.co>
2023-06-27 13:56:19 -0500

    Try dropdown

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,6 @@ var hamburguerMenu = document.querySelector(".hamburguer-menu");
 var responsiveMenu = document.querySelector(".responsive-menu");
 var closeMenu = document.querySelector(".close-drawer");
 var dropdownMenus = document.querySelectorAll(".dropdown-open");
-var dropdownMenus2 = document.querySelectorAll(".subtitulo-2");
 var activeDropdown = null;
 
 responsiveMenu.style.height = screen.height;
@@ -43,22 +42,33 @@ dropdownMenus.forEach(function (dropdownMenu) {
   });
 });
 
-dropdownMenus2.forEach(function (dropdownMenu) {
-  dropdownMenu.addEventListener("click", function () {
-    var parentLi = this.closest(".drawer-item-container");
-    var childLi = parentLi.querySelector("dropdown-mega-menu");
+// Dropdown Menu
+var dropdown = document.querySelectorAll('.subtitulo-2');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+  var button = el.querySelector('a[data-toggle="dropdown"]'),
+      menu = el.querySelector('.dropdown-menu'),
+      arrow = button.querySelector('i.icon-arrow');
 
-    if (activeDropdown && activeDropdown !== childLi) {
-      activeDropdown.style.height = "0";
+  button.onclick = function(event) {
+    if(!menu.hasClass('show')) {
+      menu.classList.add('show');
+      menu.classList.remove('hide');
+      arrow.classList.add('open');
+      arrow.classList.remove('close');
+      event.preventDefault();
     }
+    else {
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+      arrow.classList.remove('open');
+      arrow.classList.add('close');
+      event.preventDefault();
+    }
+  };
+})
 
-    if (childLi.style.height === "0px" || !childLi.style.height) {
-      childLi.style.height = childLi.scrollHeight + "px";
-      activeDropdown = childLi;
-    } else {
-      childLi.style.height = "0";
-      activeDropdown = null;
-    }
-  });
-});
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
 
