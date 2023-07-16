6cc1376f5a917f70f540876f8afbe5509c1be099
idelahoz14 <idelahoz14@cuc.edu.co>
2023-07-15 22:57:31 -0500

    trying recommended products

--- assets/main.js
+++ assets/main.js
@@ -211,36 +211,30 @@ $('.input-group').on('click', '.button-minus', function(e) {
 
 // Prueba productos recomendados
 
-var loadProductRecommendationsIntoSection = function() {
-  // Look for an element with class 'product-recommendations'
-  var productRecommendationsSection = document.querySelector(".product-recommendations");
-  if (productRecommendationsSection === null) { return; }
-  // Read product id from data attribute
-  var productId = productRecommendationsSection.dataset.productId;
-  // Read limit from data attribute
-  var limit = productRecommendationsSection.dataset.limit;
-  // Build request URL
-  var requestUrl = "/recommendations/products?section_id=product-recommendations&limit="+limit+"&product_id="+productId;
-  // Create request and submit it using Ajax
-  var request = new XMLHttpRequest();
-  request.open("GET", requestUrl);
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 300) {
-      var container = document.createElement("div");
-      container.innerHTML = request.response;
-      productRecommendationsSection.parentElement.innerHTML = container.querySelector(".product-recommendations").innerHTML;
-    }
-  };
-  request.send();
+const handleIntersection = (entries, observer) => {
+  if (!entries[0].isIntersecting) return;
+
+  observer.unobserve(productRecommendationsSection);
+
+  const url = productRecommendationsSection.dataset.url;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(text => {
+      const html = document.createElement('div');
+      html.innerHTML = text;
+      const recommendations = html.querySelector('.product-recommendations');
+
+      if (recommendations && recommendations.innerHTML.trim().length) {
+        productRecommendationsSection.innerHTML = recommendations.innerHTML;
+      }
+    })
+    .catch(e => {
+      console.error(e);
+    });
 };
-// If your section has theme settings, the theme editor
-// reloads the section as you edit those settings. When that happens, the
-// recommendations need to be fetched again.
-// See https://help.shopify.com/en/themes/development/sections/integration-with-theme-editor
-document.addEventListener("shopify:section:load", function(event) {
-  if (event.detail.sectionId === "product-recommendations") {
-    loadProductRecommendationsIntoSection();
-  }
-});
-// Fetching the recommendations on page load
-loadProductRecommendationsIntoSection();
+
+const productRecommendationsSection = document.querySelector('.product-recommendations');
+const observer = new IntersectionObserver(handleIntersection, {rootMargin: '0px 0px 200px 0px'});
+
+observer.observe(productRecommendationsSection);
